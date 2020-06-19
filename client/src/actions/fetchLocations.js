fe7d23de503b0af22231536ad4db102c4d60a17b
Pignataro67 <Pignataro67@gmail.com>
2020-06-19 05:26:32 -0700

    adding action

--- client/src/actions/fetchLocations.js
+++ client/src/actions/fetchLocations.js
@@ -1,3 +1,10 @@
+function _fetchLocation(input) {
+  debugger
+  return fetch(`/search_start/${input}`)
+  .then(response => response.json())
+  .then(locations => locations.predictions)
+}
+
 export function fetchStartingLocation(input) {
   console.log(input);
 }
