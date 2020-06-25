3f99545d15b09ea1d5880a637dee0f386b5580b7
Pignataro67 <Pignataro67@gmail.com>
2020-06-25 05:14:37 -0700

    adding reducer

--- client/src/reducers/rootreducer.js
+++ client/src/reducers/rootreducer.js
@@ -1,5 +1,10 @@
 export default function rootReducer(state = {
   startingLocation: '', destination: ''
+  suggestedDestinations: [],
+  isConvertingStartLatLong: false,
+  isConvertingDestinationLatLong: false,
+  startLatLong: '',
+  destinationLatLong: ''
 }, action) {
   switch(action.type){
     case "FETCHING_SUGGESTED_START_LOCATIONS":
@@ -8,7 +13,15 @@ export default function rootReducer(state = {
       return {...state, isFetchingDestination: true, suggestedDestinations: []}
     case "DISPLAY_DESTINATIONS":
       return {...state, isFetchingDestination: false, suggestedDestinations: action.suggestedDestinations}
+      case "CONVERTING_START_LAT_LONG":
+      return {...state, isConvertingStartLatLong: true}
+    case "RETRIEVE_START_LAT_LONG":
+      return {...state, isConvertingStartLatLong: false, startLatLong: action.startLatLong}
+      case "CONVERTING_DESTINATION_LAT_LONG":
+      return {...state, isConvertingDestiationLatLong: true}
+    case "RETRIEVE_DESTINATION_LAT_LONG":
+      return {...state, isConvertingDestinationLatLong: false, destinationLatLong: action.destinationLatLong}
     default:
-        return state
+      return state      
   }
 } 
