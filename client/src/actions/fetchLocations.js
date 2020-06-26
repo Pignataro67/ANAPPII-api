de1ae7d94f2611100383fd849e90400271d30539
Pignataro67 <Pignataro67@gmail.com>
2020-06-26 06:36:44 -0700

    anotha' one

--- client/src/actions/fetchLocations.js
+++ client/src/actions/fetchLocations.js
@@ -51,3 +51,12 @@ return async (dispatch) => {
   debugger
   }
 }
+
+export function getMapboxKey(){
+  return (dispatch) => {
+    dispatch({ type: 'FETCHING_MAPBOX_KEY' });
+    fetch("/RailsApi/confirm_route/mapbox")
+    .then(response => response.text())
+    .then(key => dispatch({ type: 'ADD_MAPBOX_KEY_TO_STATE', key }));
+  };
+}
