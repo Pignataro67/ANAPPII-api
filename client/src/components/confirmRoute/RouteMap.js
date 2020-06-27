a8770867080bbc69a60821a3c71762b76d5d23bf
Pignataro67 <Pignataro67@gmail.com>
2020-06-27 12:51:41 -0700

    adding state

--- client/src/components/confirmRoute/RouteMap.js
+++ client/src/components/confirmRoute/RouteMap.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import ReactMapboxGl, { Marker } from "react-mapbox-gl";
+import { Icon } from 'semantic-ui-react';
 
 class RouteMap extends Component {
+    state = {
+      Map: null
+    }
 
+    componentDidUpdate(prevProps) {
+      if(!prevProps.mapboxKey && this.props.mapboxKey) {
+        this.setState({
+        Map: ReactMapboxGl({
+        accessToken: this.props.mapboxKey
+        })
+      })
+    }
+  }
 }
 
 export default RouteMap;
