04f9b5bf51d631e0e5c8f40bd9adaad655ff8ba1
Pignataro67 <Pignataro67@gmail.com>
2020-06-28 09:10:23 -0700

    adding render

--- client/src/components/confirmRoute/RouteMap.js
+++ client/src/components/confirmRoute/RouteMap.js
@@ -16,6 +16,27 @@ class RouteMap extends Component {
           })
         }
       }
+
+    render() {
+      const {Map} = this.state
+
+      return Map? (
+        <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        center={[this.props.startLong, this.props.startLat]}
+        containerStyle={{
+          height: "300px",
+          width: "300px"
+        }}>
+          <Marker coordinates={[this.props.startLong, this.props.startLat]}>
+              <Icon name="circle" />
+          </Marker>
+          <Marker coordinates={[this.props.destinationLong, this.props.destinationLat]}>
+              <Icon name="circle" />
+          </Marker>
+      </Map>
+      ): null
     }
+  }
 
 export default RouteMap;
