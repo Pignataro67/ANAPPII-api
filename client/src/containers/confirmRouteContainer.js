485858583e5b8702c7ca13b6942d833442a0f82a
Pignataro67 <Pignataro67@gmail.com>
2020-06-28 09:17:02 -0700

    exporting md2p

--- client/src/containers/confirmRouteContainer.js
+++ client/src/containers/confirmRouteContainer.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import ConfirmRoute from '../components/confirmRoute/ConfirmRoute'
 import CardLabel from '../components/CardLabel';
+import Card from '../components/Card';
+import * as searchActions from '../actions/fetchLocations';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 class ConfirmRouteContainer extends Component{
 
@@ -10,10 +14,24 @@ class ConfirmRouteContainer extends Component{
         return (
       <div>
         <CardLabel cardLabel={cardLabel} />
-        <ConfirmRoute /> 
+        <ConfirmRoute {...this.props}/> 
       </div>
     )
   }
 }
 
-export default ConfirmRouteContainer;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {...bindActionCreators(searchActions, dispatch)}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, (stateProps, dispatchProps, ownProps) => {
+  return {
+    ...stateProps,
+    router: ownProps,
+    actions: dispatchProps,
+  };
+})(ConfirmRouteContainer);
