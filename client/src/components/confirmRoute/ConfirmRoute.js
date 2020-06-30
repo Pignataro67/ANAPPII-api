d7a2b9cf7e180a4228f8e03699b419cea102e42c
Pignataro67 <Pignataro67@gmail.com>
2020-06-30 06:03:07 -0700

    addind state

--- client/src/components/confirmRoute/ConfirmRoute.js
+++ client/src/components/confirmRoute/ConfirmRoute.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import Button from '../Button';
 import RouteMap from './RouteMap';
 import Card from '../Card';
+import { Redirect } from "react-router-dom";
 
 class ConfirmRoute extends Component {
-    render() {
-      return (
-      <div>
-         <Card>
+
+  state = {
+    redirectToResults: false,
+    redirectToSearch: false
+}
+
+handleRedirect = async (e) => {
+  e.preventDefault()
+  await this.props.actions.fetchUberEstimate(this.props.startLat, this.props.startLng, this.props.destinationLat, this.props.destinationLng)
+  this.setState({
+    redirectToResults: true
+  })
+}
+  
+  render() {
+    return (
+    <div>
+       <Card>
          <RouteMap mapboxKey={this.props.mapboxKey} startLngLat={this.props.startLngLat} destinationLngLat={this.props.destinationLngLat}/>
           <Button /> 
           <Button />
