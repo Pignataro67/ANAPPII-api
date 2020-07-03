1ab79dd3acb0d8053a7f51b8e26a847043305392
Pignataro67 <Pignataro67@gmail.com>
2020-07-03 06:00:37 -0700

    improving routes and actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope(:path => '/RailsApi') do
   get '/search_locations/:input', to: 'places_search#search_locations'
   get '/confirm_route/convert_lat_long/:location', to: 'geocode_map#get_lat_long'
-  get '/confirm_route/mapbox', to: 'confirm_route#mapbox'	    get '/confirm_route/mapbox', to: 'geocode_map#mapbox'
-  get '/uber_lyft/?startLat=:startLat&startLng=:startLng&destinationLat=:destinationLat&destinationLng=:destinationLng', to: 'uber_lyft#uber_estimate'
+  get '/confirm_route/mapbox', to: 'confirm_route#mapbox'
+  get '/uber_lyft', to: 'uber_lyft#uber_estimate'
   end
 end
