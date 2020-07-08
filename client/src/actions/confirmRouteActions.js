c5c1aff2c5ba28b989f624941cf1ff39ff0f5136
Pignataro67 <Pignataro67@gmail.com>
2020-07-08 05:42:22 -0700

    actions

--- client/src/actions/confirmRouteActions.js
+++ client/src/actions/confirmRouteActions.js
@@ -3,13 +3,13 @@ function _normalizeUber({ prices }){
     {type: display_name, costEstimate: estimate}
   ))
 }
-
+  
 function _normalizeLyft({ cost_estimates }){
   return cost_estimates.map(({display_name, estimated_cost_cents_min, estimated_cost_cents_max}) => (
     {type: display_name, costEstimate: `$${estimated_cost_cents_min / 100}-${estimated_cost_cents_max / 100}`}
   ))
 } 
-
+  
 export function fetchUberEstimate(startLat, startLng, destinationLat, destinationLng){
   return (dispatch) => {
     dispatch({ type: 'FETCHING_UBER_ESTIMATE' });
@@ -20,7 +20,7 @@ export function fetchUberEstimate(startLat, startLng, destinationLat, destinatio
     .then(estimates => dispatch({ type: 'ADD_UBER_ESTIMATES_TO_STATE', estimates: estimates }))
   };
 }
-
+  
 export function fetchLyftEstimate(startLat, startLng, destinationLat, destinationLng){
   return (dispatch) => {
     dispatch({ type: 'FETCHING_LYFT_ESTIMATE' });
