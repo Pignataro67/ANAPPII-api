c9142af47d83b4521a11f4dee9933b7684ff41c2
Pignataro67 <Pignataro67@gmail.com>
2020-07-12 06:44:30 -0700

    adding like/counter func.

--- client/src/components/results/TableRow.js
+++ client/src/components/results/TableRow.js
@@ -4,12 +4,23 @@ import { Table } from 'semantic-ui-react';
 
 export default class TableRow extends Component {
 
-    render() {
-      return (
-        <Table.Row>
-          <Table.Cell>{this.props.estimate.type}</Table.Cell>
-          <Table.Cell>{this.props.estimate.costEstimate}</Table.Cell>
-        </Table.Row>
-      )
-    }
+  state = {
+    counter: 0
+  }
+  
+  handleOnClick = () => {
+    this.setState((prevState, props) => {
+      return {counter: prevState.counter + 1}
+    })
+  }
+
+  render() {
+    return (
+      <Table.Row>
+        <Table.Cell>{this.props.estimate.type}</Table.Cell>
+        <Table.Cell>{this.props.estimate.costEstimate}</Table.Cell>
+        <Table.Cell><Button buttonTitle={`Like ${this.state.counter}`} onClick={ () => this.handleOnClick()}></Button></Table.Cell>
+      </Table.Row>
+    )
+  }
 }
