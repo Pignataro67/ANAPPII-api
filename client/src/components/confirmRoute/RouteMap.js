be5bbbb90f4d84660174071c8bec0047dc226762
Pignataro67 <Pignataro67@gmail.com>
2020-07-14 05:26:27 -0700

    adding comp. did mount

--- client/src/components/confirmRoute/RouteMap.js
+++ client/src/components/confirmRoute/RouteMap.js
@@ -1,45 +1,54 @@
 import React, { Component } from 'react';
 import ReactMapboxGl, { Layer, Marker } from "react-mapbox-gl";
-import { Icon } from 'semantic-ui-react';
 
 class RouteMap extends Component {
-    state = {
-      Map: null
-    }
+  state = {
+    Map: null
+  }
 
-    componentDidUpdate(prevProps) {
-      if(!prevProps.mapboxKey && this.props.mapboxKey) {
-        this.setState({
-          Map: ReactMapboxGl({
-            accessToken: this.props.mapboxKey
-          })
-          })
-        }
-      }
-
-    render() {
-      const {Map} = this.state
-
-      return Map? (
-        <Map
-        style="mapbox://styles/mapbox/streets-v9"
-        center={this.props.startLat}
-        containerStyle={{
-          height: "300px",
-          width: "400px"
-        }}>
-          <Layer 
-           type="symbol"
-           id="marker"
-           layout={{
-            'icon-image': 'star-15',
-            'icon-allow-overlap': true }}>
-        <Marker coordinates={this.props.startLngLat}/>
-        <Marker coordinates={this.props.destinationLngLat}/>
-        </Layer>
-      </Map>
-      ): null
+  componentDidUpdate(prevProps) {
+    if(!prevProps.mapboxKey && this.props.mapboxKey) {
+      this.setState({
+        Map: ReactMapboxGl({
+          accessToken: this.props.mapboxKey
+        })
+      })
     }
   }
+    
+  componentDidMount() {
+    if(this.props.mapboxKey) {
+      this.setState({
+        Map: ReactMapboxGl({
+          accessToken: this.props.mapboxKey
+        })
+      })
+    }
+  }
+
+  render() {
+    const {Map} = this.state
+
+     return Map? (
+      <Map
+      style="mapbox://styles/mapbox/streets-v9"
+      center={this.props.startLat}
+      containerStyle={{
+        height: "300px",
+        width: "400px"
+      }}>
+        <Layer 
+         type="symbol"
+         id="marker"
+         layout={{
+          'icon-image': 'star-15',
+          'icon-allow-overlap': true }}>
+      <Marker coordinates={this.props.startLngLat}/>
+      <Marker coordinates={this.props.destinationLngLat}/>
+      </Layer>
+    </Map>
+    ): null
+  }
+}
 
 export default RouteMap;
